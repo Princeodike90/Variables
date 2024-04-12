1de95753ce1ff6bbfd6e4e1b78aa06859d9380e9
Prince Odike <125614955+Princeodike90@users.noreply.github.com>
2024-04-12 11:42:51 +0100

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,35 @@
+// String 
+const theNameOfMySchool = "Rivers State University";
+const theNameOfMyPet= "Bull";
+const stateOfMyFirstOrigin = "Nigeria";
+const nameOfMyBestFood= "Garri";
+const favoriteMovieOfTheYear= "The Tribe of Judae";
+
+// Number 
+const lengthOfTheMansShoe = 25;
+const heightOfTheYoungBoy = 67;
+const sizeOfTheMeasuredBoat = 60.77;
+const weightOfTheOldWoman = 70;
+const hightOfTheSeaShore = 57799.5;
+
+// Boolean 
+const isRainingHeavilyHereNow = true;
+const obiIsNotSleepNow = false;
+const theBoyIsDancingNow = false;
+const youthriveIsSponsordByAccess = true;
+const johnHasSubmittedHisAssignment = true;
+
+// Null 
+let mySchoolExamScoreVariable1 = null;
+let distanceBetweenHeavenAndEarth = null;
+let myFavouritDancingHorseSing = null;
+let myStateOfMyOrigine = null;
+const myYouthriveHidenScoreArray1 = [1, null, 3, null, 5];
+
+
+// Undefined 
+let mySchoolExamScoreVariable;
+let myYouthriveHidenScoreArray = [1, 2, 3];
+let undefinedLocationOfNewProject;
+let undefinedMyFavouritColourBe;
+let undefinedMyPrimarySchoolTeacher;
